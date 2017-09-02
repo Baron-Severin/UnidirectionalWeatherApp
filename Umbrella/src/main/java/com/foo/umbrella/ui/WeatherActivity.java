d52a4fd1c36d230d69f3e580805ba27fcc502dc1
Erik Rudie <severinrudie@gmail.com>
2017-09-02 11:03:26 -0700

    Updated Gradle; created early project structure. Have not yet seperated existing classes into new packages

--- Umbrella/src/main/java/com/foo/umbrella/ui/WeatherActivity.java
+++ Umbrella/src/main/java/com/foo/umbrella/ui/WeatherActivity.java
@@ -4,10 +4,13 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import com.foo.umbrella.R;
 
-public class MainActivity extends AppCompatActivity {
+public class WeatherActivity extends AppCompatActivity {
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+
+    
   }
 }

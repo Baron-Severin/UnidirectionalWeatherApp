6d27220e25f94076871261d3b8ec53b6fb6a1a9b
Erik Rudie <severinrudie@gmail.com>
2017-09-02 12:13:40 -0700

    Early (hardcoded, unpolished) layout top portion written

--- Umbrella/src/main/java/com/foo/umbrella/ui/WeatherActivity.java
+++ Umbrella/src/main/java/com/foo/umbrella/ui/WeatherActivity.java
@@ -3,14 +3,17 @@ package com.foo.umbrella.ui;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import com.foo.umbrella.R;
+import com.foo.umbrella.data.ApiServicesProvider;
+
+import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
 
 public class WeatherActivity extends AppCompatActivity {
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    setContentView(R.layout.activity_weather);
 
-
-    
   }
 }
